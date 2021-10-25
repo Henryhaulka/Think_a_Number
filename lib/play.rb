6010d96fa4e01540f6839a5916764dcc5c35a717
Henry Onu <onuhenry24@gmail.com>
2021-10-25 15:55:40 +0100

    Add play.rb

---
+++ lib/play.rb
@@ -0,0 +1,86 @@
+class Game
+    attr_reader :min, :max, :secret_guess, :live
+    
+    def initialize(min = nil,max = nil)
+        @min = min
+        @max = max
+        @secret_guess = rand(@min..@max)
+        @live = ((@min..@max).size.to_f/2).ceil
+        @play = true
+    end
+    
+    def hint(guess)
+        if guess > secret_guess
+            puts 'Number too big'
+        elsif guess == secret_guess
+            puts 'Correct guess!!!'
+        else
+            puts 'Number too small'
+        end
+    end
+
+    def live_update
+        puts "In this game, you will have #{live} lives"
+    end
+    
+    def game_on
+        live_update
+        while @play
+            puts "Input a number between #{min} and #{max}"
+            guess = gets.chomp.to_i
+            p secret_guess
+            hint(guess)
+            puts "_ _ _ _ _ _"
+            puts
+            winner(guess)
+        end
+        
+    end
+
+    def winner(guess)
+        if secret_guess == guess and live >= 0
+            puts "You win"
+            @play = false
+        elsif secret_guess != guess and live > 0
+            #Here we are not re-assign (even if we use a writer method), we are performing an operatio
+            #hence why we didn't use 'live' but @live
+            @live -= 1
+            puts "You have #{live} lives remaining"
+             
+        else
+            puts "You lose"
+           @play = false
+       end
+    end   
+end
+# puts "Welcome, in this game, you input 2 numbers(max & min)"
+# puts "You try to get the number i am thinking of"
+# puts "Enter a maximum number for your RANGE"
+# max = gets.chomp.to_i
+# puts "Enter a minimum number for your RANGE"
+# min = gets.chomp.to_i
+# secret_guess = ''
+# 1.times do
+#     secret_guess = rand(min..max)
+# end
+# live = 3
+
+# game_on = true
+# while game_on
+#         puts "Input a number between #{min} and #{max}"
+#         guess = gets.chomp.to_i
+#         p secret_guess
+#     if secret_guess.to_i == guess and live >= 0
+#         puts "You win"
+#         game_on = false
+
+#     elsif live == 0 && secret_guess.to_i != guess
+#             puts "You lose"
+#             game_on = false
+                
+#     elsif secret_guess.to_i != guess && live <=3
+#     live -= 1
+#     puts "You have #{live} lives remaining"
+    
+#     end    
+# end
