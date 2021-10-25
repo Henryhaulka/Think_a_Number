21343d75848ad8ed41f614b2c50a1561bcbe53b0
Henry Onu <onuhenry24@gmail.com>
2021-10-25 16:21:10 +0100

    Fix rubocop error

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,23 @@
-require_relative "../lib/play"
+require_relative '../lib/play'
 class Player
-    def name_checks(name)
-      !name.empty?
-    end
+  def name_checks(name)
+    !name.empty?
+  end
 end
-  
-        puts "Welcome, in this game, you input 2 numbers(max & min)"
-        puts "You try to get the number i am thinking of"
-        puts 'Enter a valid name'
-        valid = false
-        until valid
-            name = gets.chomp
-            valid = Player.new.name_checks(name)
-            puts "Name should not be blank" if !valid
-         end
-          
-        puts "Enter a maximum number for your RANGE"
-        max = gets.chomp.to_i
-        puts "Enter a minimum number for your RANGE"
-        min = gets.chomp.to_i
 
+puts 'Welcome, in this game, you input 2 numbers(max & min)'
+puts 'You try to get the number i am thinking of'
+puts 'Enter a valid name'
+valid = false
+until valid
+  name = gets.chomp
+  valid = Player.new.name_checks(name)
+  puts 'Name should not be blank' unless valid
+end
 
+puts 'Enter a maximum number for your RANGE'
+max = gets.chomp.to_i
+puts 'Enter a minimum number for your RANGE'
+min = gets.chomp.to_i
 
-Game.new(min,max, name).game_on
+Game.new(min, max, name).game_on
--- lib/play.rb
+++ lib/play.rb
@@ -1,59 +1,58 @@
 class Game
-    attr_reader :min, :max, :secret_guess, :live, :name
-    
-    def initialize(min = nil,max = nil, name)
-        @min = min
-        @max = max
-        @secret_guess = rand(@min..@max)
-        @live = ((@min..@max).size.to_f/2).ceil
-        @play = true
-        @name = name
-    end
-    
-    def hint(guess)
-        if guess > secret_guess
-            puts 'Number too big'
-        elsif guess == secret_guess
-            puts 'Correct guess!!!'
-        else
-            puts 'Number too small'
-        end
-    end
+  attr_reader :min, :max, :secret_guess, :live, :name
 
-    def live_update
-        puts "In this game, you will have #{live} lives"
-    end
-    
-    def game_on
-        live_update
-        while @play
-            puts
-            puts "I am thinking of a between #{min} and #{max}"
-            puts "Enter a Number"
-            guess = gets.chomp.to_i
-            hint(guess)
-            puts "_ _ _ _ _ _"
-            puts
-            winner(guess)
-        end
-        
-    end
+  def initialize(min, max, name)
+    @min = min
+    @max = max
+    @secret_guess = rand(@min..@max)
+    @live = ((@min..@max).size.to_f / 2).ceil
+    @play = true
+    @name = name
+  end
 
-    def winner(guess)
-        if secret_guess == guess and live >= 0
-        puts "#{name} win!!!"
-            @play = false
-        elsif secret_guess != guess and live > 0
-            #Here we are not re-assign (even if we use a writer method), we are performing an operatio
-            #hence why we didn't use 'live' but @live
-            @live -= 1
-        puts "#{name} have #{live} lives remaining"
-             
-        else
-        puts "#{name} lose"
-           @play = false
-       end
-    end   
+  def hint(guess)
+    if guess > secret_guess
+      puts 'Number too big'
+    elsif guess == secret_guess
+      puts 'Correct guess!!!'
+    else
+      puts 'Number too small'
+    end
+  end
+
+  def live_update
+    puts "In this game, you will have #{live} lives"
+  end
+
+  def game_on
+    live_update
+    while @play
+      puts
+      puts "I am thinking of a between #{min} and #{max}"
+      puts 'Enter a Number'
+      guess = gets.chomp.to_i
+      hint(guess)
+      puts '_ _ _ _ _ _'
+      puts
+      winner(guess)
+    end
+  end
+
+  def winner(guess)
+    if secret_guess == guess and live >= 0
+      puts "#{name} win!!!"
+      @play = false
+    elsif secret_guess != guess and live.positive?
+      # Here we are not re-assign (even if we use a writer method), we are performing an operatio
+      # hence why we didn't use 'live' but @live
+      @live -= 1
+      puts "#{name} have #{live} lives remaining"
+
+    else
+      puts "#{name} lose"
+      @play = false
+    end
+  end
 end
 # puts "Welcome, in this game, you input 2 numbers(max & min)"
 # puts "You try to get the number i am thinking of"
@@ -79,10 +78,10 @@ end
 #     elsif live == 0 && secret_guess.to_i != guess
 #             puts "You lose"
 #             game_on = false
-                
+
 #     elsif secret_guess.to_i != guess && live <=3
 #     live -= 1
 #     puts "You have #{live} lives remaining"
-    
-#     end    
+
+#     end
 # end
